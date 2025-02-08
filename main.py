21e5568d4cd003ef93f454264e7469e00cbc8820
JuanD508 <dinosaurioguerrillero@gmail.com>
2025-02-08 09:30:55 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import time
+meme_dict = {
+    "CRINGE": "Algo excepcionalmente raro o embarazoso",
+    "LOL": "Una respuesta común a algo gracioso",
+    "ROFL": "Una respuesta a una broma",
+    "SHEESH": "Una respuesta a una broma",
+    "CREEPY": "Aterrador, siniestro",
+    "AGGRO": "Ponerse agresivo o enojado"
+}
+
+for i in range(5):
+    word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+    if word in meme_dict:
+        print(meme_dict[word])
+    else:
+        print("Esa palabra no está en el diccionario")
+    time.sleep(1)
